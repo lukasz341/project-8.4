2bb662221d3eb168eea7b0837f368a7f7148965e
lukasz341 <lukasz341@tlen.pl>
2017-08-25 16:34:07 +0200

    Dodanie komentarzy

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var name = prompt('Enter your name');//przypisanie do zmiennej wartości wprowadzonej z ekranu
+/*wyświetlanie wcześniej wprowadzonej zmiennej
+jako alert i w konsoli*/
+alert('Hello, ' + name);//wyświetlenie wprowadzonej zmiennej z ekranu 
+console.log('Hello, ' + name);// wyświetlenie w konsoli wcześniej wprowadzonej z ekranu zminnej
